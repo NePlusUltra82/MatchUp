28d41dd28724d61afa0a98d19aaebd7cc06ffee0
Derrell Page <derrellpage@gmail.com>
2019-02-12 16:08:41 -0800

    backup

--- main.js
+++ main.js
@@ -7,78 +7,132 @@ $(function(){
     
     const LASTFM_USER_FRIENDS_URL="http://ws.audioscrobbler.com/2.0/?method=user.getfriends&user=YOUR_USER_NAME&limit=500&api_key=YOUR_API_KEY&format=json"
 
+    const friendArray = [];
+
+    let SCORE = 0;
+    let QUESTIONS = 0;
+
 
     $('.js-intro').click(event => {
         event.preventDefault();
         $('.js-intro').remove();
         renderGame();
     });
-    
-    function renderFeedback(){
-          
+
+    function updateScore() {
+        SCORE++;
+        console.log(`Current Score: ${SCORE}/${QUESTIONS}`);   
     }
 
-    function updateScore(){
+    function renderFeedback(data1, data2){
+        $('.answer-form').toggleClass('hidden');
 
-        /*
+        $('.js-results').toggleClass('hidden');
 
-        This should provide feedback based on correct/incorrect answer. If incorrect, it should list what friend actually listened to 
+        if (data1 === data2 ){
+            console.log('correct');
+            updateScore();
+        }
+        else if (data2 === 'images/default-album-image.jpg') {
+            console.log('correct')
+            updateScore();
+
+
+        }
+        else {
+            console.log('incorrect');
+            console.log(`Current Score: ${SCORE}/${QUESTIONS}`);
+        }
+        
+       
+       /* This should provide feedback based on correct/incorrect answer. If incorrect, it should list what friend actually listened to 
         the album immediately and update the total score then proceed to next album. Perhaps use an animation that flips the album image
         around to a blank colored background with the actual user's profile image displayed and then remove it from the DOM and adjust/
         rearrange the remaining albums and repeat until NO albums are left ---only a FINAL feedback page listing the overall score
-        maybe create: renderFinalFeedback() for this purpose?
-        would also be cool if you click one album to match and the rest disappear until the user initially answers and feedback is given
-        have something that looks like a disheveled stack of vinyl records in the background ---but using actual covers from the albums list via last.fm
-        OR start the game off with the vinyl stack and answer each one by one off the top of the stack and animate the album spinning off the stack after it flips
-        over and reveals the feedback
-        Resources: http://animista.net/play/exits/swirl-out/swirl-out-bottom-fwd
-                    https://designshack.net/articles/css/use-pseudo-elements-to-create-an-image-stack-illusion/
-
-        */
-        
-
-
+        maybe create: renderFinalFeedback() for this purpose? */
+          
     }
+    
+    
+    function displayAnswers(){ 
+        //recieves an array of user topalbum objects. displays the username and corresponding top album in a select menu for the user to use for selecting their answer
+        $('.album').click( function(){
+            const selectedAlbum = $(this).attr('src');
+            $('.js-results').toggleClass('hidden');
 
-    function displayAllAlbums(data){
+            $('.photos').html(
 
-            /* 
-            
-             Need to find a way to randomize the order of images displayed to the user ---
-                Should the image URLs be randomized OR the data from friends list URL? 
-             
-             Also, how to make the page responsive to the size of the viewport AND the number of albums to display.
-
-             Example --- What if the user only has (3) friends aka (3) albums to display...
-                How can the web app adjust the image display or size without distorting the image?
-
-             How to EXCLUDE -- ALL UNDEFINED/INACTIVE Users AND BROKEN/MISSING IMAGES (set default image with title listed for this case?) from the game aka friendlist/top albums searches/results? 
-                (Use an if statement?)
-
-             Any way to implement an 'infinite scroll' display that automatically loads more album images as the user scrolls down the page 
-                ---similar to twitter/facebook feeds?   
-
-             Need to ADD a displayAllFriends() that shows the profile name/real name and profile pic of all friends
-                ---then I need to SPLIT the screen in order to show the list of albums and list of users to match on each side.
-
-                ---OR create a visualization that will display a branched chain with a # of Users connected to an album and 
-                    from this visualization be able to click on a Users pic/name as a way to match/answer then provide instant
-                    FEEDBACK and SCORE UPDATE --- EXAMPLE: https://www.conceptdraw.com/How-To-Guide/what-is-a-circle-spoke-diagram
-             */
-
-        $('.js-gameform').remove();
-        data.forEach(element => {
-            $('.js-results').addClass('active').append(
-                `
-                   <img  aria-live="assertive" class='flip-vertical-fwd' src=${element.image[3]['#text']} alt="${element['name']} by ${element.artist['name']}" title="${element['name']} by ${element.artist['name']}"/> 
+                ` 
+                <div class='answer-form'>
+                <form class='working'>
+                    <label for="answer-select">Choose the username associated with this album:</label>
+                    <img  aria-live="assertive" class='album no-style' src=${selectedAlbum} />
+                    <select id="answer-select">
+                        <option value="">--Please choose an option--</option>
+                    </select>
+                    </div>
+                    </form>
                 `
             );
-        });
-    }
 
-   // $('img').click(function(){
-     //   $(this).toggleClass('flip-vertical-fwd','clicked');
-   // });
+           // $('.answer-form').toggleClass('hidden');
+
+            friendArray.forEach(element => {
+                $('#answer-select').append(
+                    `
+                        <option value="${element.topalbums.album[0].image[3]['#text']}">${element.topalbums['@attr']['user']}</option>
+                    `
+                );
+            }); 
+
+            $('#answer-select').change(() => {
+                const userAnswer = $('#answer-select').val();
+                $(this).submit();    
+                event.preventDefault();
+                console.log(userAnswer);
+                renderFeedback(selectedAlbum, userAnswer); 
+            });
+            $(this).toggleClass('hidden');
+
+        });
+
+    } 
+
+    function displayAllAlbums(data){
+        $('.js-gameform').remove(); 
+        data.forEach(element => {
+            const albumArtURL = element.topalbums.album[0].image[3]['#text'];
+            const albumName = element.topalbums.album[0]['name'];
+            const albumArtist = element.topalbums.album[0].artist['name'];
+            if (albumArtURL === ""){
+                //displays DEFAULT album art if there is none avail/listed on last.fm api
+              /*  $('.js-results').addClass('active').append(
+                    `
+                    <img  aria-live="assertive" class='album' src='images/default-album-image.jpg' alt="${albumName} by ${albumArtist}" title="${albumName} by ${albumArtist}"/> 
+                    `
+                );
+                
+                */
+                //increments when each photo is rendered ---provides the total # of photos aka questions
+               // QUESTIONS++;
+                return;
+            }
+            //Skips over any albums that may have incomplete/mislabed metadata 
+            else if (albumName === 'MP3' && albumArtist === '[unknown]'){
+                return;
+            }
+            else {
+                //displays official album covers
+                $('.js-results').addClass('active').append(
+                    `
+                    <img  aria-live="assertive"  class='album' src=${albumArtURL} alt="${albumName} by ${albumArtist}" title="${albumName} by ${albumArtist}"/> 
+                    `
+                );
+                QUESTIONS++;
+            } 
+        });
+       displayAnswers();
+    }
 
     function getFriendAlbumUrl(friends){
         friends.forEach(element => {
@@ -88,19 +142,23 @@ $(function(){
                 .replace('YOUR_USER_NAME', element)
                 .replace('DESIRED_TIMEFRAME', timeframe)
             $.getJSON(albumUrl)
-                //.then(json => console.log([json.topalbums.album[0].image[3]['#text']]));
-                .then(json => displayAllAlbums([json.topalbums.album[0]]))
+                .then(json => {
+                    if (json.topalbums['@attr'].total !== '0'){
+                    displayAllAlbums([json]);
+                    friendArray.push(json);
+                    }
+                    else {
+                        return;
+                    }
+                })
         });
     }
 
     function getUserFriendList(data){
-
-        //Need to create a way to EXCLUDE any friends with no recent data aka they have NO top albums
-
        const friendList = data.friends.user.map(element => {
             return element.name;    
         });
-        getFriendAlbumUrl(friendList);
+        getFriendAlbumUrl(friendList);     
     }
     
     function getTimeframe(){
@@ -108,48 +166,19 @@ $(function(){
     }
     
     function getUserName(){
-
-        //Need to create a test/prompt to the user if the Last.fm profile name entered is invalid
-
         return $('#username').val();
     }
 
     function renderGame(){
-        $('.js-results').html(
-        `   <form action="" role="form" class='js-gameform' aria-live="polite">
-                <label for="username">Last.fm User Name:</label>
-                <input type="text" id="username" size='35' required placeholder="Enter a Last.fm Profile Name Here">
-                <fieldset>
-                    <legend>Choose a timeframe:</legend>
-                    <label for="week">Within Last Week:</label>
-                    <input type="radio" id="week" name="timeframe" value="7day" required>
-                    <label for="month">Within Last Month:</label>
-                    <input type="radio" id="month"  name="timeframe" value="1month" required>
-                    <label for="year">Within Last Year:</label>
-                    <input type="radio" id="year" name="timeframe" value="12month" required>
-                    <label for="overall">Overall:</label>
-                    <input type="radio" id="overall" name="timeframe" value="overall" required>
-                </fieldset>
-                <button type="submit">Submit</button>
-            </form>
-        `);
+        const html = $('#entry-form-tpl').html()
+        $('.js-results').html($('#entry-form-tpl').html());
+
         $('.js-gameform').submit(ev => {
             ev.preventDefault();
             const username = getUserName();
-
-            let userData;
-
-            // Username lookup
-            $.getJSON(`http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${username}&api_key=${access}&format=json`)
-                .then(payload => {
-                    userData = payload.user
-
-                    const url = LASTFM_USER_FRIENDS_URL
-                        .replace('YOUR_API_KEY', access)
-                        .replace('YOUR_USER_NAME', username);
-
-                    return $.getJSON(url)
-                })
+            
+            userLookup(username)
+                .then(() => getUserFriends(username))
                 .then(json => getUserFriendList(json))
                 .catch(err => {
                     if (err.status === 404) {
@@ -161,5 +190,26 @@ $(function(){
                     }
                 })
         });
+    }
+
+    function getUserFriends (username) {
+        const url = LASTFM_USER_FRIENDS_URL
+            .replace('YOUR_API_KEY', access)
+            .replace('YOUR_USER_NAME', username);
+
+        return $.getJSON(url)
+    }
+
+    function userLookup (user) {
+        const params = Object.entries({
+            method: 'user.getinfo',
+            api_key: access,
+            format: 'json',
+            user
+        })
+            .map(([key, val]) => `${key}=${val}`)
+            .join('&')
+        
+        return $.getJSON(`http://ws.audioscrobbler.com/2.0/?${params}`)
     }
 });
